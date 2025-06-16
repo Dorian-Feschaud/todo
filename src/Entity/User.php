7f359a16df92931c7ff5d873dbfa3486017436d9
Dorian Feschaud <dorian.feschaud@gmail.com>
2025-06-16 12:08:31 +0200

    fix asserts

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,8 +10,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -24,6 +24,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?String $username = null;
 
     #[ORM\Column(length: 64)]
+    #[Assert\NotBlank(message: 'Vous devez saisir un mot de passe.')]
     private ?String $password = null;
 
     /**

e9c1272ebe74615bb28f9a1e23177072d8859482
Dorian Feschaud <dorian.feschaud@gmail.com>
2025-06-19 14:51:32 +0200

    isGranted ROLE_ADMIN #5

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,7 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class UserController extends AbstractController
 {
 
